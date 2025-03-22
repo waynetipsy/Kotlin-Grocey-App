964abe8ecdc7cf7d7957bf3b260ae5f84bbc5660
waynetipsy <jacketoniru@gmail.com>
2025-03-22 15:16:35 +0100

    Last Push

---
+++ app/src/main/java/app/ify/kotlingroceyapp/Item.kt
@@ -0,0 +1,7 @@
+package app.ify.kotlingroceyapp
+
+data class Item(
+    val tile: String,
+    val desc: String,
+    val image: Int
+)
--- app/src/main/java/app/ify/kotlingroceyapp/MainActivity.kt
+++ app/src/main/java/app/ify/kotlingroceyapp/MainActivity.kt
@@ -1,13 +1,18 @@
 package app.ify.kotlingroceyapp
 
+import android.media.RouteListingPreference.Item
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -17,6 +22,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,7 +35,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LazyColumnWithStickyHeader()
+            LazyColumnWithStickyHeader( )
         }
         }
     }
@@ -36,25 +43,40 @@ class MainActivity : ComponentActivity() {
  @OptIn(ExperimentalFoundationApi::class)
  @Composable
  fun LazyColumnWithStickyHeader() {
-     val groupedItems = mapOf(
-         //1 - Creating the data source : map with key-value pairs
-         "Fruits" to listOf("Apple", "Banana", "Cherry", "Mango", "Grapes", "WaterMelon"),
-         "vegetables" to listOf("Carrot", "Lettuce", "Tomato","Onions","Potato","Eggplant")
-     )
-     val myfruits = groupedItems["Fruits"]
 
-     // Lazy Column
+     //1 - Creating the data source : map with key-value pairs
+    val groupedItems = mapOf(
+        "Fruits" to listOf(
+            Item("Apple","A sweet red fruit",R.drawable.apple),
+            Item("Banana", "A long yellow", R.drawable.bananas),
+            Item("Cherry", "A small red fruit",R.drawable.cherry),
+            Item("Mango", "A declious Fruit", R.drawable.mango),
+            Item("Waterlemon", "A big and sweet Fruit", R.drawable.watermelon),
+            Item("Grapes", "A Tasty Fruit", R.drawable.grapes),
+            ),
+        "Vegetables" to listOf(
+            Item("Carrot", "A long orange vegetable", R.drawable.carrot),
+            Item("Lettuce", "A leafy green vegetable", R.drawable.lettuce),
+            Item("Broccoli", "A beautiful vegetable", R.drawable.brocoli),
+            Item("Onion", "Amazing vegetable", R.drawable.onion),
+            Item("Potato", "A yellow vegetable", R.drawable.potato),
+            Item("Tomato", "A red and sweet vegetable", R.drawable.tomato),
+            Item("Pea", "A sweet vegetable", R.drawable.pea)
+    )
+    )
+
      LazyColumn {
          groupedItems.forEach {
              (header, items) ->
              stickyHeader {
-           MyCustomHeader(title = header)
+                 MyCustomHeader(title = header)
              }
-             items (items){
-                 item -> MyCustomItem(itemTitle = item)
+             items(items){
+                 item -> MyCustomItem(item)
              }
          }
      }
+
  }
 
   @Composable
@@ -63,20 +85,34 @@ class MainActivity : ComponentActivity() {
          text = title,
          fontSize = 32.sp,
          modifier = Modifier.fillMaxWidth()
-             .background(Color.Red)
+             .background(Color.Green)
              .padding(8.dp)
 
      )
   }
 
    @Composable
-   fun MyCustomItem(itemTitle: String){
-       Card {
-           Text(text = itemTitle,
-               modifier = Modifier.padding(8.dp),
-               fontSize = 24.sp
-           )
-       }
+   fun MyCustomItem(item: app.ify.kotlingroceyapp.Item){
+      Card (modifier = Modifier
+          .fillMaxWidth().padding(8.dp)){
+          Column(modifier = Modifier.padding(16.dp))  {
+              Image( painter = painterResource(id = item.image),
+                  contentDescription = "Item Image",
+                  modifier = Modifier.fillMaxWidth()
+                      .height(180.dp),
+                  contentScale = ContentScale.Crop
+                  )
+              Spacer(modifier = Modifier.height(8.dp))
+
+                Text(text = item.tile,
+                    fontSize = 22.sp)
+
+              Spacer(modifier = Modifier.height(4.dp))
+
+              Text(text = item.desc, fontSize = 18.sp)
+
+          }
+      }
    }
 
 
